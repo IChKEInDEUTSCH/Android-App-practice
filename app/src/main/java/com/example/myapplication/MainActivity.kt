42cff0f98be87c83c41b18f12286838f1252d19a
SILENCER45 <102silencer@gmail.com>
2023-06-04 16:53:03 +0800

    some random changes

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,16 +1,33 @@
 package com.example.myapplication
 
 import android.Manifest
+import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.budiyev.android.codescanner.*
 
+
 class MainActivity : AppCompatActivity() {
     private lateinit var codeScanner: CodeScanner
+    private lateinit var openExcel : Button
+    var resultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val myData: Intent? = result.data
+                if (myData != null) {
+                    Log.d("MainActivity", "MY_DATA:" + myData.getStringExtra("Success"))
+                }
+            }
+        }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +37,14 @@ class MainActivity : AppCompatActivity() {
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA),1001)
         }
         val scannerView = findViewById<CodeScannerView>(R.id.scanner_view)
+        openExcel = findViewById(R.id.FindExcelButton)
+        openExcel.setOnClickListener {
+            val intent = Intent()
+                .setType("*/*")
+                .setAction(Intent.ACTION_GET_CONTENT)
 
+            resultLauncher.launch(intent)
+        }
         codeScanner = CodeScanner(this, scannerView)
 
         // Parameters (default values)
@@ -59,4 +83,5 @@ class MainActivity : AppCompatActivity() {
         codeScanner.releaseResources()
         super.onPause()
     }
+
 }
